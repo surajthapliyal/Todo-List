3ce470d4819068ee7c9091a06b208b1142a7fbf0
Suraj <surajthapliyal18@gmail.com>
2021-08-06 14:35:58 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import TodoForm from "./Components/todoform";
+import TodoList from "./Components/todolist";
+
+function App() {
+  return (
+    <div className="todo-app">
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
